45ee594ca633132b3539e6385a768b9c38fdf9b0
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2016-02-29 19:14:58 -0300

    Added initial version of master / details components implementation

---
+++ src/EmployeeApp/wwwroot/js/detailsGrid.ts
@@ -0,0 +1,32 @@
+﻿import {Component} from 'angular2/core';
+import {AgGridNg2} from 'ag-grid-ng2/main';
+import {IItemInfo} from './itemInfo';
+
+@Component({
+    selector: 'details-grid',
+    templateUrl: '../html/detailsGrid.html',
+    directives: [AgGridNg2],
+    inputs: ['itemInfo']
+})
+export class DetailsGridComponent {
+    itemInfo: IItemInfo;
+
+    columnDefs = [
+        { headerName: "Make", field: "make" },
+        { headerName: "Model", field: "model" },
+        {
+            headerName: "Price",
+            field: "price",
+            cellClass: 'rightJustify',
+            cellRenderer: function (params: any) {
+                return '$' + params.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            }
+        }
+    ];
+    // put data directly onto the controller
+    rowData = [
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Mondeo", price: 32000 },
+        { make: "Porsche", model: "Boxter", price: 72000 }
+    ];
+}
