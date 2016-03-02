63745376d9370055ce6abce92f4cf26ba1df656b
Jon Galloway <jongalloway@gmail.com>
2016-03-02 02:05:40 -0800

    Added comments to orderdetails model.

--- src/OrderApp/Models/OrderDetails.cs
+++ src/OrderApp/Models/OrderDetails.cs
@@ -5,6 +5,7 @@
         public int OrderDetailsId { get; set; }
         public virtual Product Product { get; set; }
         public int Quantity { get; set; }
+        public string Comments { get; set; }
         public virtual Order Order { get; set; }
     }
 }
